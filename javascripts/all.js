74bd718ad1acc03853928f2abd7fa2ca3ee43ed0
evenwu <evendesign@gmail.com>
2013-09-26 00:17:10 +0800

    nav

---
+++ javascripts/all.js
@@ -0,0 +1,18 @@
+function isMobile(){
+  return (
+    (navigator.userAgent.match(/Android/i)) ||
+    (navigator.userAgent.match(/webOS/i)) ||
+    (navigator.userAgent.match(/iPhone/i)) ||
+    (navigator.userAgent.match(/iPod/i)) ||
+    (navigator.userAgent.match(/iPad/i)) ||
+    (navigator.userAgent.match(/BlackBerry/))
+  );
+}
+
+
+// 如果不是手持裝置
+if(!isMobile()) {
+
+  $(".nav-inner").affix();
+
+}
