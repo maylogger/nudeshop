256a902888ea43e6fe0de0b8acab63d735203a86
shengli1989 <shengli@evendesign.tw>
2013-10-09 23:39:50 +0800

    微調細節

--- config.rb
+++ config.rb
@@ -27,7 +27,6 @@ line_comments = false
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
 
-
 the_hold_options = {
   :login => "evenwu", # 你的帳號
   :token => "229f2b40d4f17f98f142318250a75f20", # 你的密碼
@@ -36,3 +35,18 @@ the_hold_options = {
   :host => "http://the-hold.handlino.com/",
   :cname => "nudeshop.evendesign.tw"
 }
+
+css_dir = "stylesheets" # by Fire.app 
+sass_dir = "sass" # by Fire.app 
+images_dir = "images" # by Fire.app 
+javascripts_dir = "javascripts" # by Fire.app 
+fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app 
+fireapp_livescripts_dir = "livescripts" # by Fire.app 
+fireapp_minifyjs_on_build = false # by Fire.app 
+fireapp_always_report_on_build = true # by Fire.app 
+output_style = :compact # by Fire.app 
+relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
+sass_options = {:debug_info=>false} # by Fire.app 
+fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
+fireapp_livescript_options = {:bare=>false} # by Fire.app 
