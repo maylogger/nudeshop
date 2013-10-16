b2c8ec9806a2de6d9a4de7a532488a893e329a4d
shengli1989 <shengli@evendesign.tw>
2013-10-16 18:14:01 +0800

    add lightbox plugin

--- config.rb
+++ config.rb
@@ -44,7 +44,7 @@ fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app
 fireapp_livescripts_dir = "livescripts" # by Fire.app 
 fireapp_minifyjs_on_build = true # by Fire.app 
 fireapp_always_report_on_build = true # by Fire.app 
-output_style = :compressed # by Fire.app 
+output_style = :expanded # by Fire.app 
 relative_assets = false # by Fire.app 
 line_comments = false # by Fire.app 
 sass_options = {:debug_info=>false} # by Fire.app 
